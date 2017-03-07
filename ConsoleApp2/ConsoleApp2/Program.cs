6e270bf49f0b67c011ae2b04741a4b2245276931
xiasimon <xia.simon@gmail.com>
2017-03-07 14:22:03 -0600

    added the print line

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //commect added
+            Console.WriteLine("Main function finished");
         }
     }
 }
